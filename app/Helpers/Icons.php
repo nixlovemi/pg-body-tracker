1ac163cc2935f409aa1b888c69f5ad6f4052214c
Leandro Parra <nixlovemi@gmail.com>
2025-02-14 21:00:06 -0500

    feat: Icons class

---
+++ app/Helpers/Icons.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Helpers;
+
+final class Icons {
+    public const INFO_CIRCLE = '<i class="fas fa-info-circle"></i>';
+    public const GOOGLE = '<i class="fab fa-google fa-fw"></i>';
+    public const PLUS = '<i class="fas fa-plus"></i>';
+    public const WEIGHT = '<i class="fas fa-weight"></i>';
+    public const BARS = '<i class="fa fa-bars"></i>';
+    public const SIGN_OUT_GREY = '<i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>';
+    public const ANGLE_UP = '<i class="fas fa-angle-up"></i>';
+    public const EYE = '<i class="fa-solid fas fa-eye fa-fw"></i>';
+    public const EDIT = '<i class="fa-solid fas fa-pencil-alt fa-fw"></i>';
+}
